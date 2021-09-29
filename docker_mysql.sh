6bcb38a6f6dfac552ca08115a5c344979c93da14
songqx <songqx@microport.com>
2021-09-29 09:02:59 +0800

    add mysql

--- docker_mysql.sh
+++ docker_mysql.sh
@@ -1,6 +1,1 @@
-docker run --name morningstar -e MYSQL_ROOT_PASSWORD=morningstar  \
-       -e MYSQL_USER=morningstar \
-       -e MYSQL_PASSWORD=morningstar \
-       -e MYSQL_DATABASE=morningstar \
-       -p 3306:3306 \
-       -d mysql:8.0.26
+docker run --name morningstar -e MYSQL_ROOT_PASSWORD=morningstar  -e MYSQL_USER=morningstar -e MYSQL_PASSWORD=morningstar -e MYSQL_DATABASE=morningstar -p 3308:3306 -d mysql:8.0.26
